ffcf553788b6c6dbaf478df777f139d64f01ee27
TMilley4k <tyoungmoneymills@gmail.com>
2023-11-13 19:47:15 -0500

    Fixed Bullshit

--- WebSite/lib/scripts/generated.js
+++ WebSite/lib/scripts/generated.js
@@ -1,1 +1,1 @@
-let nodes={paths:["geopolitics/levant-charts/israel-&-palestine.html","geopolitics/levant-1919-2006-notes.html"],nodeCount:2,linkSources:[],linkTargets:[],labels:["Israel & Palestine","Levant 1919-2006 Notes"],radii:[null,null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
+let nodes={paths:["index.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["index"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
